a600f8ed48a2730e1fc928a9f00b94595c76ff72
contactspeedycleans-afk <contactspeedycleans@gmail.com>
2026-07-22 10:40:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -846,8 +846,8 @@ Greet ${customer?.first_name || 'the customer'} warmly by first name, ask the si
                 initializeSession();
             });
 
-openAiWs.on('message', async (data) => {                try {
-                    const response = JSON.parse(
+openAiWs.on('message', async (data) => {
+    try {                    const response = JSON.parse(
                         data.toString()
                     );
 
