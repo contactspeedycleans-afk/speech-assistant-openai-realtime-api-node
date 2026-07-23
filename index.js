a80a77f10081d52d6df68882935a0b712b1c04a4
contactspeedycleans-afk <contactspeedycleans@gmail.com>
2026-07-22 22:48:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -893,19 +893,7 @@ if (
                             })
                         );
 
-console.log(
-    'Function DONE payload:',
-    JSON.stringify(response, null, 2)
-);
 
-if (
-    response.type ===
-        'response.function_call_arguments.done' &&
-    response.name ===
-        'search_company_knowledge'
-) {
-    ...
-}
                         
                     }
 if (
